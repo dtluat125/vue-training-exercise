75b965352ef4d5b44d1addf7606e50343f4dd262
Luat <you@example.com>
2022-09-07 09:58:04 +0700

    SVG import debug

--- src/assets/svg.d.ts
+++ src/assets/svg.d.ts
@@ -1,4 +1,5 @@
 declare module "*.svg" {
-  const src: string;
-  export default src;
+  import Vue, { VueConstructor } from "vue";
+  const content: VueConstructor<Vue>;
+  export default content;
 }
